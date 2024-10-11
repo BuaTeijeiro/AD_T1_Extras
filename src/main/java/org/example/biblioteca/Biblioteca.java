34e6a26647456efa634bce7d03400ec8c503a8de
a23davidbt <bua.teijeiro.david@gmail>
2024-10-11 13:23:41 +0200

    feat(Biblioteca.java): añadir funcion de añadir publicaciones a la biblioteca

--- src/main/java/org/example/biblioteca/Biblioteca.java
+++ src/main/java/org/example/biblioteca/Biblioteca.java
@@ -1,16 +1,20 @@
 package org.example.biblioteca;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
     private List<Publicacion> publicaciones;
 
     public Biblioteca() {
+        publicaciones = new ArrayList<Publicacion>();
     }
 
     public List<Publicacion> getPublicaciones() {
         return publicaciones;
     }
 
-
+    public void addPublicacion(Publicacion publicacion) {
+        this.publicaciones.add(publicacion);
+    }
 }
--- src/main/java/org/example/biblioteca/Main.java
+++ src/main/java/org/example/biblioteca/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
 
-        Publicacion libro = new Libro("978-2-3255-4452-0", "La Sombra del Viento", 2002);
-        Publicacion revista = new Revista("978-2-6756-3580-0","Science",2024,248);
+        Libro libro = new Libro("978-2-3255-4452-0", "La Sombra del Viento", 2002);
+        Revista revista = new Revista("978-2-6756-3580-0","Science",2024,248);
+
+        biblioteca.addPublicacion(libro);
+        biblioteca.addPublicacion(revista);
+
+        System.out.println(biblioteca.getPublicaciones());
 
 
     }
