8e8b5e7243ffe9a588dc37cc8fad8b47dfd08a4e
a23davidbt <bua.teijeiro.david@gmail>
2024-10-11 13:18:51 +0200

    feat(Main.java): crear main básico

--- src/main/java/org/example/biblioteca/Biblioteca.java
+++ src/main/java/org/example/biblioteca/Biblioteca.java
@@ -11,4 +11,6 @@ public class Biblioteca {
     public List<Publicacion> getPublicaciones() {
         return publicaciones;
     }
+
+
 }
---
+++ src/main/java/org/example/biblioteca/Main.java
@@ -0,0 +1,12 @@
+package org.example.biblioteca;
+
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        Publicacion libro = new Libro("978-2-3255-4452-0", "La Sombra del Viento", 2002);
+        Publicacion revista = new Revista("978-2-6756-3580-0","Science",2024,248);
+
+
+    }
+}
