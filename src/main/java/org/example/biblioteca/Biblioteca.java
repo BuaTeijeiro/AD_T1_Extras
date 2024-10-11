d816c15d889092dcb072f3023b944f0d03145e3e
a23davidbt <bua.teijeiro.david@gmail>
2024-10-11 13:15:15 +0200

    feat(Biblioteca.java): Crear clase biblioteca

---
+++ src/main/java/org/example/biblioteca/Biblioteca.java
@@ -0,0 +1,14 @@
+package org.example.biblioteca;
+
+import java.util.List;
+
+public class Biblioteca {
+    private List<Publicacion> publicaciones;
+
+    public Biblioteca() {
+    }
+
+    public List<Publicacion> getPublicaciones() {
+        return publicaciones;
+    }
+}
